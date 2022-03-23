d6e35dafa68016429bf61ab969c79888ec2fb7c1
Schaffer <mr.giancarloschaffer@gmail.com>
2022-03-22 23:47:14 -0300

    create pwa functions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import { ref, onValue } from "firebase/database";
 import axios from "axios";
 import logo from "./logo.png";
 import "./App.scss";
-import { Checkbox, Rate, Tag, Tooltip, Popover, Card, Divider } from "antd";
+import {
+  Checkbox,
+  Rate,
+  Tag,
+  Tooltip,
+  Popover,
+  Card,
+  Divider,
+  message,
+} from "antd";
 import Countdown from "react-countdown";
 import Icon from "./Icon";
 import { LoadingOutlined } from "@ant-design/icons";
@@ -212,8 +221,10 @@ function App() {
 
   useEffect(() => {
     const dark = localStorage.getItem("dark-mode");
-    if (dark) {
+    const body = document.querySelector("body");
+    if (dark === "true") {
       setDarkMode(dark === "true");
+      body.classList.add("dark-mode");
     }
     // eslint-disable-next-line
   }, []);
@@ -226,16 +237,92 @@ function App() {
 
   const handleDarkMode = () => {
     setDarkMode(!darkMode);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-mode", !darkMode);
+
     localStorage.setItem("dark-mode", !darkMode);
   };
 
+  function copyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+
+    //
+    // *** This styling is an extra step which is likely not required. ***
+    //
+    // Why is it here? To ensure:
+    // 1. the element is able to have focus and selection.
+    // 2. if the element was to flash render it has minimal visual impact.
+    // 3. less flakyness with selection and copying which **might** occur if
+    //    the textarea element is not visible.
+    //
+    // The likelihood is the element won't even render, not even a
+    // flash, so some of these are just precautions. However in
+    // Internet Explorer the element is visible whilst the popup
+    // box asking the user for permission for the web page to
+    // copy to the clipboard.
+    //
+
+    // Place in the top-left corner of screen regardless of scroll position.
+    textArea.style.position = "fixed";
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+
+    // Ensure it has a small width and height. Setting to 1px / 1em
+    // doesn't work as this gives a negative w/h on some browsers.
+    textArea.style.width = "2em";
+    textArea.style.height = "2em";
+
+    // We don't need padding, reducing the size if it does flash render.
+    textArea.style.padding = 0;
+
+    // Clean up any borders.
+    textArea.style.border = "none";
+    textArea.style.outline = "none";
+    textArea.style.boxShadow = "none";
+
+    // Avoid flash of the white box if rendered for any reason.
+    textArea.style.background = "transparent";
+
+    textArea.value = text;
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+      var successful = document.execCommand("copy");
+      var msg = successful ? "successful" : "unsuccessful";
+      console.log("Copying text command was " + msg);
+    } catch (err) {
+      console.log("Oops, unable to copy");
+    }
+
+    document.body.removeChild(textArea);
+  }
+
+  const handleShare = () => {
+    copyTextToClipboard(
+      `Eu já assisti esses aqui: ${movies
+        .filter((e) => e.watched)
+        .map((e) => e.movie.name)
+        .join(", ")}\n\nMarque você também em https://oscars.netlify.app`
+    );
+    message.success("Texto copiado!");
+  };
+
   return (
-    <div className={`oscar-body${darkMode ? " dark-mode" : ""}`}>
+    <div className='oscar-body'>
       <span
         onClick={handleDarkMode}
         className='material-icons-outlined dark-button'
       >
         {darkMode ? "light_mode" : "dark_mode"}
+      </span>
+      <span
+        onClick={handleShare}
+        className='material-icons-outlined share-button'
+      >
+        ios_share
       </span>
       <header className='oscar-header'>
         <img src={logo} className='oscar-logo' alt='oscar-logo' />
