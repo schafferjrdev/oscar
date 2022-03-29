7088af61472fd9b58391656333c1ae29c35d95a0
Schaffer <mr.giancarloschaffer@gmail.com>
2022-03-29 11:41:58 -0300

    modify icon to ios and android

--- src/App.js
+++ src/App.js
@@ -368,6 +368,13 @@ function App() {
     }
   };
 
+  const detectIOS = () => {
+    const toMatch = [/iPhone/i, /iPad/i, /iPod/i];
+    return toMatch.some((toMatchItem) => {
+      return navigator.userAgent.match(toMatchItem);
+    });
+  };
+
   return (
     <div className='oscar-body'>
       <span
@@ -380,7 +387,7 @@ function App() {
         onClick={handleShare}
         className='material-icons-outlined share-button'
       >
-        ios_share
+        {detectIOS() ? "ios_share" : "share"}
       </span>
       <header className='oscar-header'>
         <img src={logo} className='oscar-logo' alt='oscar-logo' />
