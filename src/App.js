324964b409fc61c54e19c56b34f2fc6e37f3dbe6
Schaffer <mr.giancarloschaffer@gmail.com>
2024-02-05 10:11:12 -0300

    redesign update to oscar page

--- src/App.js
+++ src/App.js
@@ -4,25 +4,23 @@ import { ref, onValue } from "firebase/database";
 import axios from "axios";
 import logo from "./logo.png";
 import "./App.scss";
-import {
-  Checkbox,
-  Rate,
-  Tag,
-  Tooltip,
-  Popover,
-  Card,
-  Divider,
-  message,
-} from "antd";
+import { message, Drawer, Rate, Popover, Tag } from "antd";
 import Countdown from "react-countdown";
 import Icon from "./Icon";
-import { LoadingOutlined } from "@ant-design/icons";
-import { CATEGORIES, LOCAL_STORAGE_KEY } from "./constants";
-
-const { Meta } = Card;
+import {
+  LoadingOutlined,
+  CloseOutlined,
+  EyeFilled,
+  EyeInvisibleFilled,
+  StarFilled,
+} from "@ant-design/icons";
+import CaretDown from "./icons/CaretDown.svg";
+import { LOCAL_STORAGE_KEY, CATEGORIES } from "./constants";
 
 const MovieCard = ({ handleRate, handleCheck, data, index }) => {
   const [omdb, setOmdb] = useState(null);
+  const [tmdb, setTmdb] = useState(null);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const Categories = ({ list = [] }) => {
     return (
@@ -40,11 +38,23 @@ const MovieCard = ({ handleRate, handleCheck, data, index }) => {
     );
   };
 
+  const showDrawer = (e) => {
+    console.log("openning drawer");
+    e.preventDefault();
+    setDrawerOpen(true);
+  };
+
+  const onClose = (e) => {
+    console.log("closing drawer");
+    e.preventDefault();
+    setDrawerOpen(false);
+  };
+
   const getOMDB = async (uuid) => {
     await setOmdb(null);
-
+    const imdb = uuid?.split("/")[4];
     axios
-      .get(`https://www.omdbapi.com/?apikey=81750ce2&i=${uuid?.split("/")[4]}`)
+      .get(`https://www.omdbapi.com/?apikey=81750ce2&i=${imdb}`)
       .then(function (response) {
         // handle success
         setOmdb(response.data);
@@ -55,114 +65,325 @@ const MovieCard = ({ handleRate, handleCheck, data, index }) => {
       });
   };
 
+  const getTMDB = async (uuid) => {
+    await setTmdb(null);
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OWUyMWU1NWNjMTg0YzBmNTBkYjc4Njk1NzlhYWE3MCIsInN1YiI6IjY0NDAwZDc1MzdiM2E5MDQ0NTQzMmZhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uB0qMp0SyCG2Lph-6EUJK4eopBlIurD7SdBw8bTb_Uw",
+      },
+    };
+    const imdb = uuid?.split("/")[4];
+
+    axios
+      .get(
+        `https://api.themoviedb.org/3/find/${imdb}?external_source=imdb_id&language=pt-BR`,
+        options
+      )
+      .then(function (response) {
+        // handle success
+        setTmdb(response.data?.movie_results[0]);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     getOMDB(data?.movie.imdb);
+    getTMDB(data?.movie.imdb);
   }, [data]);
 
   return (
-    <Card
-      cover={
-        omdb?.Poster ? (
-          <img
-            onClick={() =>
-              handleCheck(index, { target: { checked: !data?.watched } })
-            }
-            className='poster-image'
-            alt='movie_poster'
-            src={omdb?.Poster}
-            title='Clique para marcar que viu'
-          />
+    <div className={`new-card ${data?.watched ? " checked" : ""}`}>
+      <Drawer
+        placement='bottom'
+        onClose={onClose}
+        closable={false}
+        open={drawerOpen}
+      >
+        <div className='movie-details'>
+          <div className='drawer-backdrop mobile'>
+            <div className='drawer-actions'>
+              <span
+                className={`checkbox-watch ${data?.watched ? "" : "not-seen"}`}
+                onClick={() => handleCheck(index)}
+              >
+                {data?.watched ? <EyeFilled /> : <EyeInvisibleFilled />}
+              </span>
+              <img
+                onClick={(e) => onClose(e)}
+                src={CaretDown}
+                alt='Icon Caret Down'
+              />
+            </div>
+            <div className='backdrop-drawer-image'></div>
+            <img
+              className='drawer-image'
+              alt='movie_poster'
+              src={
+                !!tmdb?.backdrop_path
+                  ? `https://image.tmdb.org/t/p/original${tmdb?.backdrop_path}`
+                  : omdb?.Poster
+              }
+            />
+
+            <div className='drawer-header'>
+              <p className='drawer-name'>
+                <span>
+                  <b className='drawer-main'>{data?.movie.name}</b>
+                  <span className='drawer-sub'>{omdb?.Year}</span>
+                </span>
+                <span>
+                  <i className='drawer-sub'>{omdb?.Title}</i>
+                </span>
+              </p>
+              <Rate
+                allowHalf
+                allowClear
+                onChange={(val) => handleRate(index, val)}
+                value={data?.rate}
+              />
+            </div>
+            <div className='banner-information'>
+              <div className='banner-upper'>
+                <p className='banner-subtitle'>
+                  {omdb?.Runtime} • {omdb?.Genre}
+                </p>
+              </div>
+              <div className='movie-plot'>
+                <p>
+                  <span className='banner-body'>{tmdb?.overview}</span>
+                </p>
+                {data?.platform.url ? (
+                  <>
+                    <hr />
+                    <p className='banner-topic'>WATCH NOW</p>
+
+                    <Icon type={data?.platform.name} url={data?.platform.url} />
+                  </>
+                ) : null}
+                <hr />
+                <div>
+                  <p className='banner-topic'>CAST & CREW</p>
+                  <p>
+                    <b>Director</b>
+                    <span className='banner-body'>{omdb?.Director}</span>
+                  </p>
+                  <p>
+                    <b>Writers</b>
+                    <span className='banner-body'>{omdb?.Writer}</span>
+                  </p>
+                  <p>
+                    <b>Stars</b>
+                    <span className='banner-body'>{omdb?.Actors}</span>
+                  </p>
+
+                  <hr />
+                  <p className='banner-topic awards'>
+                    <span>AWARDS & NOMANATIONS</span>
+                    <Popover
+                      title='Indicações'
+                      content={<Categories list={data?.category} />}
+                      placement='left'
+                      className='movie-indications'
+                    >
+                      {data?.category && (
+                        <Tag
+                          color={CATEGORIES[data?.category[0]]?.color}
+                          className='tag-major'
+                        >
+                          {CATEGORIES[data?.category[0]]?.title}
+                        </Tag>
+                      )}
+                    </Popover>
+                  </p>
+                  <p>
+                    <b>Oscar</b>
+                    <span className='banner-body'>
+                      {data?.category?.length} indicações
+                    </span>
+                  </p>
+                  <p>
+                    <b>Premiações</b>
+                    <span className='banner-body'>{omdb?.Awards}</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='movie-banner desktop'>
+            <CloseOutlined
+              className='close-button'
+              onClick={(e) => onClose(e)}
+            />
+
+            <img
+              className='banner-image'
+              alt='movie_poster'
+              src={
+                !!tmdb?.backdrop_path
+                  ? `https://image.tmdb.org/t/p/original${tmdb?.backdrop_path}`
+                  : omdb?.Poster
+              }
+            />
+            <div className='banner-backdrop'>
+              <div className='banner-content'>
+                <img
+                  className='banner-poster-image'
+                  alt='movie_poster'
+                  src={
+                    !!tmdb?.poster_path
+                      ? `https://image.tmdb.org/t/p/original${tmdb?.poster_path}`
+                      : omdb?.Poster
+                  }
+                />
+                <span
+                  className={`checkbox-watch ${
+                    data?.watched ? "" : "not-seen"
+                  }`}
+                  onClick={() => handleCheck(index)}
+                >
+                  {data?.watched ? <EyeFilled /> : <EyeInvisibleFilled />}
+                </span>
+                <div className='banner-information'>
+                  <div className='banner-upper'>
+                    <span className='banner-title'>
+                      <span className='banner-name'>{data?.movie.name}</span>
+                      <span>
+                        <Rate
+                          allowHalf
+                          allowClear
+                          onChange={(val) => handleRate(index, val)}
+                          value={data?.rate}
+                        />
+                      </span>
+                    </span>
+                    <span className='banner-subtitle'>
+                      {omdb?.Title} • {omdb?.Year} • {omdb?.Runtime} •{" "}
+                      {omdb?.Genre}
+                    </span>
+                  </div>
+                  <div className='movie-plot'>
+                    <p>
+                      <span className='banner-body'>{tmdb?.overview}</span>
+                    </p>
+                    {data?.platform.url ? (
+                      <>
+                        <hr />
+                        <p className='banner-topic'>WATCH NOW</p>
+
+                        <Icon
+                          type={data?.platform.name}
+                          url={data?.platform.url}
+                        />
+                      </>
+                    ) : null}
+                    <hr />
+                    <div>
+                      <p className='banner-topic'>CAST & CREW</p>
+                      <p>
+                        <b>Director</b>
+                        <span className='banner-body'>{omdb?.Director}</span>
+                      </p>
+                      <p>
+                        <b>Writers</b>
+                        <span className='banner-body'>{omdb?.Writer}</span>
+                      </p>
+                      <p>
+                        <b>Stars</b>
+                        <span className='banner-body'>{omdb?.Actors}</span>
+                      </p>
+
+                      <hr />
+                      <p className='banner-topic awards'>
+                        <span>AWARDS & NOMANATIONS</span>
+                        <Popover
+                          title='Indicações'
+                          content={<Categories list={data?.category} />}
+                          placement='left'
+                          className='movie-indications'
+                        >
+                          {data?.category && (
+                            <Tag
+                              color={CATEGORIES[data?.category[0]]?.color}
+                              className='tag-major'
+                            >
+                              {CATEGORIES[data?.category[0]]?.title}
+                            </Tag>
+                          )}
+                        </Popover>
+                      </p>
+                      <p>
+                        <b>Oscar</b>
+                        <span className='banner-body'>
+                          {data?.category?.length} indicações
+                        </span>
+                      </p>
+                      <p>
+                        <b>Premiações</b>
+                        <span className='banner-body'>{omdb?.Awards}</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Drawer>
+      <div onClick={(e) => showDrawer(e)}>
+        {tmdb?.poster_path || omdb?.Poster ? (
+          <div className='poster-image-section'>
+            <img
+              className='poster-image'
+              alt='movie_poster'
+              src={
+                !!tmdb?.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${tmdb?.poster_path}`
+                  : omdb?.Poster
+              }
+              title='Clique para marcar que viu'
+            />
+            <span
+              className={`checkbox-watch ${data?.watched ? "" : "not-seen"}`}
+            >
+              {data?.watched ? <EyeFilled /> : <EyeInvisibleFilled />}
+            </span>
+          </div>
         ) : (
           <LoadingOutlined />
-        )
-      }
-      loading={!omdb}
-      style={{ width: "100%" }}
-      className={`movie-card${data?.watched ? " checked" : ""}`}
-      actions={
-        // data?.subtitle
-        //   ? [
-        //       <Icon type={data?.platform.name} url={data?.platform.url} />,
-        //       <Icon
-        //         type={data?.subtitle ? "legenda" : null}
-        //         url={data?.subtitle}
-        //       />,
-        //     ]
-        //   : [<Icon type={data?.platform.name} url={data?.platform.url} />]
-        data?.platform.url
-          ? [<Icon type={data?.platform.name} url={data?.platform.url} />]
-          : null
-      }
-    >
-      <Meta
-        title={
-          <span className='watched'>
-            <a
-              href={data?.movie.imdb}
-              target='_blank'
-              rel='noreferrer'
-              title='Ir ao imdb'
-            >
-              <b>{data?.movie.name}</b>
-            </a>
+        )}
+      </div>
+      <div className='card-data'>
+        <p className='card-title'>
+          <span className='card-title-name'>{data?.movie.name} </span>
+          <span className='card-title-year'>{omdb?.Year}</span>
+        </p>
+        <p className='card-subtitle'>
+          {omdb?.Runtime} • {data?.category?.length} indicações
+          {data?.rate > 0 ? (
+            <>
+              {" "}
+              • <span> {data?.rate} </span> <StarFilled />
+            </>
+          ) : null}
+        </p>
 
-            <Tooltip title='Sua nota pessoal' placement='right'>
-              <span>
-                <Rate
-                  allowHalf
-                  allowClear
-                  onChange={(val) => handleRate(index, val)}
-                  value={data?.rate}
-                />
-              </span>
-            </Tooltip>
-          </span>
-        }
-        description={`${omdb?.Title} • ${omdb?.Year} • ${omdb?.Runtime}`}
-      />
-      <div>
-        <div className='movie-plot'>
-          <p>
-            <b>Sinopse</b>
-            {omdb?.Plot}
-          </p>
-          <p>
-            <b>Elenco</b>
-            {omdb?.Actors}
-          </p>
+        <p className='plot-text'>
+          {!!tmdb?.overview ? tmdb?.overview : omdb?.Plot}
+        </p>
+        <div>
+          {data?.platform.url ? (
+            <Icon type={data?.platform.name} url={data?.platform.url} />
+          ) : null}
         </div>
-        <Divider />
-
-        <Popover
-          title='Indicações'
-          content={<Categories list={data?.category} />}
-          placement='topRight'
-          className='movie-indications'
-        >
-          <span className='nominees-count'>
-            Indicações: {data?.category?.length}
-          </span>
-
-          {data?.category && (
-            <Tag
-              color={CATEGORIES[data?.category[0]]?.color}
-              className='tag-major'
-            >
-              {CATEGORIES[data?.category[0]]?.title}
-            </Tag>
-          )}
-        </Popover>
       </div>
-      <div className={`movie-checker${data?.watched ? " watch-checked" : ""}`}>
-        <Checkbox
-          onChange={(e) => handleCheck(index, e)}
-          checked={data?.watched}
-          className='watch-checkbox'
-        >
-          {data?.watched ? null : "Já viu?"}
-        </Checkbox>
-      </div>
-    </Card>
+    </div>
   );
 };
 
@@ -216,11 +437,10 @@ function App() {
     });
   };
 
-  const handleCheck = (index, e) => {
-    const val = e.target.checked;
+  const handleCheck = (index) => {
     setMovies((prevState) => {
       const newState = [...prevState];
-      newState[index].watched = val;
+      newState[index].watched = !newState[index].watched;
       localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState));
       return newState;
     });
@@ -394,7 +614,6 @@ function App() {
       </span>
       <header className='oscar-header'>
         <img src={logo} className='oscar-logo' alt='oscar-logo' />
-        <span>Checklist para o Oscar 2024</span>
 
         <span className='countdown-span'>
           <Countdown
