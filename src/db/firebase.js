969151406a06532a788091a295eccea42ea778da
Schaffer <mr.giancarloschaffer@gmail.com>
2025-12-21 22:15:49 -0300

    feat: added env vars

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -7,13 +7,13 @@ import { getAuth, signInAnonymously } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFGokMQx-_stk9CEJ_V8wCG-5N-nKXE70",
-  authDomain: "filmin-f90ad.firebaseapp.com",
-  projectId: "filmin-f90ad",
-  storageBucket: "filmin-f90ad.firebasestorage.app",
-  messagingSenderId: "30845965692",
-  appId: "1:30845965692:web:64ea0636897bc2ede980f9",
-  measurementId: "G-KW6131SQPT",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
